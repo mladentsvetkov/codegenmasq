cf89793973b176621238e23fdae61abec0010834
Mladen Tsvetkov <mladen.tsvetkov@gmail.com>
2023-05-19 18:52:09 +0300

    dictionary in vs code extensions config

--- codegenmasq/src/extension.ts
+++ codegenmasq/src/extension.ts
@@ -2,37 +2,51 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 
+// Function to replace words in a string
+function replaceWords(text: string, replacements: {[key: string]: string}): string {
+	let result = text;
+	for (let word in replacements) {
+			const regex = new RegExp(word, 'g');
+			result = result.replace(regex, replacements[word]);
+	}
+	return result;
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('codegenmasq.cgmasq', async () => {
+    let disposable = vscode.commands.registerCommand('codegenmasq.cgmasq', async () => {
 
-		const {activeTextEditor} = vscode.window;
+        const {activeTextEditor} = vscode.window;
 
-		if (activeTextEditor) {
-			const document = activeTextEditor.document;
-			const selection = activeTextEditor.selection;
-			const text = document.getText(selection);
-			const newSnippet = text + ' ---> my modifications';
+        if (activeTextEditor) {
+            const document = activeTextEditor.document;
+            const selection = activeTextEditor.selection;
+            const text = document.getText(selection);
 
-			// Write the original and modified text to temporary files
-			const originalFilePath = path.join(context.extensionPath, 'original.txt');
-			const modifiedFilePath = path.join(context.extensionPath, 'modified.txt');
-			fs.writeFileSync(originalFilePath, text);
-			fs.writeFileSync(modifiedFilePath, newSnippet);
+            const replacements = vscode.workspace.getConfiguration('codegenmasq').get('replacements', {});
 
-			// Open the files as TextDocuments
-			const originalDocument = await vscode.workspace.openTextDocument(originalFilePath);
-			const modifiedDocument = await vscode.workspace.openTextDocument(modifiedFilePath);
+            // Replace words in the selected text
+            let modifiedText = replaceWords(text, replacements);
 
-			// Open a diff editor comparing the original and modified documents
-			vscode.commands.executeCommand('vscode.diff', originalDocument.uri, modifiedDocument.uri, 'Original <-> Modified');
-		} else {
-			vscode.window.showInformationMessage('Active editor is undefined');
-		}
+            // Write the original and modified text to temporary files
+            const originalFilePath = path.join(context.extensionPath, 'original.txt');
+            const modifiedFilePath = path.join(context.extensionPath, 'modified.txt');
+            fs.writeFileSync(originalFilePath, text);
+            fs.writeFileSync(modifiedFilePath, modifiedText);
 
-	});
+            // Open the files as TextDocuments
+            const originalDocument = await vscode.workspace.openTextDocument(originalFilePath);
+            const modifiedDocument = await vscode.workspace.openTextDocument(modifiedFilePath);
 
-	context.subscriptions.push(disposable);
+            // Open a diff editor comparing the original and modified documents
+            vscode.commands.executeCommand('vscode.diff', originalDocument.uri, modifiedDocument.uri, 'Original <-> Modified');
+        } else {
+            vscode.window.showInformationMessage('Active editor is undefined');
+        }
+
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
